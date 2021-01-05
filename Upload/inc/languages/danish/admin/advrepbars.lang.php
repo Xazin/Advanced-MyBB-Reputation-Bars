9757768f91899ea431f72192c69ea356514b6269
Mathias M <mathiasrieckm@gmail.com>
2021-01-05 16:08:50 +0100

    Danish Language

---
+++ Upload/inc/languages/danish/admin/advrepbars.lang.php
@@ -0,0 +1,46 @@
+<?php
+
+/* PAGE DETAILS */
+$l['advrepbars_title'] = "Advanceret Omdømme Barer";
+$l['advrepbars_title_acronym'] = "Adv. Omdømme Barer";
+
+$l['advrepbars_manage'] = "Administrér";
+$l['advrepbars_manage_desc'] = "Denne side giver dig adgang til at administrere omdømme barer ved enten at slette eller ændre dem.";
+
+$l['advrepbars_new'] = "Ny Omdømme Bar";
+$l['advrepbars_new_desc'] = "This page allows you to create a new reputation bar.";
+
+$l['advrepbars_edit'] = "Redigér Omdømme Bar";
+$l['advrepbars_edit_desc'] = "Denne side giver dig mulighed for at redigere en eksisterende omdømme bar.";
+
+$l['advrepbars_page_not_found'] = "Uventet fejl: Handlingen som du forsøgte, at få adgang til, findes ikke.";
+
+/* FORM DETAILS */
+$l['advrepbars_form_new_title'] = "Tilføj ny omdømme bar";
+
+$l['advrepbars_form_edit_title'] = "Redigér en eksisterende omdømme bar";
+
+/* ERRORS */
+$l['advrepbars_form_input_missing'] = "Enten navn, niveau eller baggrundsfarve manglede. Prøv igen";
+$l['advrepbars_form_delete_error'] = "Kunne ikke slette omdømme baren, baren findes ikke";
+$l['advrepbars_form_edit_error'] = "Omdømme baren findes ikke og kan ikke redigeres";
+
+/* SUCCESS */
+$l['advrepbars_form_new_success'] = "Omdømme baren blev tilføjet";
+$l['advrepbars_form_delete_success'] = "Omdømme baren blev slettet";
+
+/* INPUTS */
+$l['advrepbars_form_input_name'] = "Navn";
+$l['advrepbars_form_input_name_desc'] = "Giv dit omdømmelinje et navn, dette bruges kun til legendesiden <a href=\"../advrepbars.php\">set her</a>";
+
+$l['advrepbars_form_input_level'] = "Niveau";
+$l['advrepbars_form_input_level_desc'] = "Det mindste omdømme, som en bruger skal opnå, for at have denne omdømme bar";
+
+$l['advrepbars_form_input_bgcolor'] = "Baggrundsfarve";
+$l['advrepbars_form_input_bgcolor_desc'] = 'Indtast en hex-farvekode eller <a href="https://developer.mozilla.org/da-US/docs/Web/CSS/linear-gradient">tjek lineære gradienter</a><em> Ekskluder \'background:\' </em>';
+
+$l['advrepbars_form_input_fontstyle'] = "Tekst Styling";
+$l['advrepbars_form_input_fontstyle_desc'] = "Tilpas <span> tagget der indeholder mængden af omdømme en person har på deres bar.";
+
+$l['advrepbars_form_submit_save'] = "Gem Omdømme Bar";
+
---
+++ Upload/inc/languages/danish/repbars_18.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$l['repbars_18_reputation'] = "Omdømme";
+$l['repbars_18_reputation_bar'] = "Omdømmelinje";
+$l['repbars_18_reputation_bars'] = "Omdømme barer";
+$l['repbars_18_reputation_bars_legend'] = "Legende for Omdømme Barer";
+$l['repbars_18_no_reputation_bars'] = "Der eksistere ikke nogen omdømme barer";
